7e2c93bdb394fc015c5e8e8e3660a3a2e775958e
Abdelghani Roussi <abdelghani_roussi_1@carrefour.com>
2022-03-16 22:14:44 +0100

    Cqrs setup

--- techbank.cmd/src/main/java/com/aroussi/cqrs/techbank/cmd/CommandApplication.java
+++ techbank.cmd/src/main/java/com/aroussi/cqrs/techbank/cmd/CommandApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommandApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommandApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class, args);
+    }
 
 }
